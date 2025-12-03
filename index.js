d1ecf284787ed7977c81aebdf8e8d392159bc2ad
Daksh03 <daxsathwara102@gmail.com>
2025-12-03 18:38:26 +0530

    cors change

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ app.use(cors())
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST"],
   })
 );
-const PORT = 4000;
+const PORT = process.env.PORT || 3000;
 ConnecetDB();
 
 app.post("/register", async (req, res) => {
